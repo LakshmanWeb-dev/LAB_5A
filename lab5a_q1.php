968927ce62da7868e5d138b07a12c62585e51b28
LakshmanWeb-dev <lakshmanlakshman1693@gmail.com>
2024-11-28 21:04:49 +0800

    Add files via upload

---
+++ lab5a_q1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 5a Q1</title>
+</head>
+<body>
+    <?php 
+        $name = "Your Name";
+        $matric_no = "Your Matric Number";
+        $course = "Your Course";
+        $year = "Year of Study";
+        $address = "Your Address";
+    ?>
+
+    <table border="1">
+        <tr>
+            <td>Name</td>
+            <td><?php echo "LAKSHMAN NATHAN" ; ?></td> 
+        </tr>
+        <tr>
+            <td>Matric Number</td>
+            <td><?php echo "CI230119" ?></td>
+        </tr>
+        <tr>
+            <td>Course</td>
+            <td><?php echo "BIS" ?></td>
+        </tr>
+        <tr>
+            <td>Year of Study</td>
+            <td><?php echo "2nd Year" ?></td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td><?php echo "NO.9 Jalan Bendahara 10/4, SELANGOR" ?></td>
+        </tr>
+    </table>
+    
+</body>
+</html>
